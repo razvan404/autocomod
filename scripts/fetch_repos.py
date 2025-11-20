3cb004ca0007b7ccec5edba962de3dd49a388037
razvan404 <razvanpetec@gmail.com>
2025-11-20 09:31:26 +0200

    Parallelize the git fetching script

--- scripts/fetch_repos.py
+++ scripts/fetch_repos.py
@@ -1,6 +1,7 @@
 import os
-import sys
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
+import pandas as pd
 from git import Repo, CommandError
 
 from autocomod.settings import Settings
@@ -9,36 +10,39 @@ from autocomod.logger import logger
 
 class FetcherSettings(Settings):
     repos_dir: str = "repos/"
-    repos_urls: str | list[str] | None = None
-    repos_urls_file: str = "files/default_repo_list.txt"
+    repos_file: str = "files/repos.csv"
+    max_workers: int = 12
+
+
+def clone_repo(repo_url: str, dest: str) -> None:
+    if os.path.exists(dest):
+        logger.info(f"{repo_url} already exists in `{dest}`, skipping.")
+        return
+
+    logger.info(f"Fetching {repo_url} in `{dest}`.")
+    try:
+        Repo.clone_from(repo_url, dest, depth=1)
+        logger.info(f"Finished fetching {repo_url} in `{dest}`.")
+    except CommandError as exc:
+        logger.error(f"Failed fetching {repo_url}: {exc}")
+        os.rmdir(dest)
 
 
 def fetch_repos(settings: FetcherSettings) -> None:
-    if settings.repos_urls is None:
-        logger.info(f"Using repos located at `{settings.repos_urls_file}`.")
-        with open(settings.repos_urls_file, "r") as f:
-            repos_urls = [line.strip() for line in f.readlines()]
-    else:
-        logger.info("Using repos mentioned through the `--repos_urls` argument.")
-        repos_urls = settings.repos_urls
-        if isinstance(repos_urls, str):
-            repos_urls = [repos_urls]
-
+    repos = pd.read_csv(settings.repos_file)
     os.makedirs(settings.repos_dir, exist_ok=True)
 
-    for repo_url in repos_urls:
-        repo_name = repo_url.split("/")[-1].replace(".git", "")
-        dest = os.path.join(settings.repos_dir, repo_name)
-        if os.path.exists(dest):
-            logger.info(f"{repo_url} already exists in `{dest}`, skipping.")
-            continue
+    with ThreadPoolExecutor(max_workers=settings.max_workers) as executor:
+        futures = []
+        for _, repo_data in repos.iterrows():
+            repo_name, repo_url = str(repo_data.id), str(repo_data.url)
+            dest = os.path.join(settings.repos_dir, repo_name)
 
-        logger.info(f"Fetching {repo_url} in `{dest}`.")
-        try:
-            Repo.clone_from(repo_url, dest, depth=1)
-            logger.info(f"Finished fetching {repo_url} in `{dest}`.")
-        except CommandError as exc:
-            logger.error(f"Failed fetching {repo_url}: {exc}")
+            future = executor.submit(clone_repo, repo_url, dest)
+            futures.append(future)
+
+        for future in as_completed(futures):
+            future.result()
 
 
 if __name__ == "__main__":
